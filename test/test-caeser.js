421206dccb4117d13548ca9842954885d1db5776
rgrannell1 <r.grannell2@gmail.com>
2014-07-07 04:03:09 +0100

    added basic code

---
+++ test/test-caeser.js
@@ -0,0 +1,21 @@
+
+const jCheck      = require('jCheck')
+
+const describe    = require('jCheck').describe
+
+const over        = require('jCheck').over
+const over_       = require('jCheck').over_
+
+const holdsWhen   = require('jCheck').holdsWhen
+const holdsWhen_  = require('jCheck').holdsWhen_
+
+const failsWhen   = require('jCheck').failsWhen
+const failsWhen_  = require('jCheck').failsWhen_
+
+const worksWhen   = require('jCheck').worksWhen
+const worksWhen_  = require('jCheck').worksWhen_
+
+const run         = require('jCheck').run
+
+
+
